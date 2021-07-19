d36778524d9cb40b66ca01d39aeb4065d99fe1ed
Muhummud Faadhil Yusuf <muhummud@webhi.co>
2021-07-20 01:32:05 +0200

    added system tests

--- test/system/projects_test.rb
+++ test/system/projects_test.rb
@@ -3,6 +3,7 @@ require "application_system_test_case"
 class ProjectsTest < ApplicationSystemTestCase
   setup do
     @project = projects(:one)
+    sign_in users(:regular)
   end
 
   test "visiting the index" do
@@ -12,31 +13,36 @@ class ProjectsTest < ApplicationSystemTestCase
 
   test "creating a Project" do
     visit projects_url
-    click_on "New Project"
+    click_on "Add New Project"
 
     fill_in "Title", with: @project.title
     click_on "Create Project"
+    assert_selector "h1", text: @project.title
 
     assert_text "Project was successfully created"
-    click_on "Back"
+    click_on "All Projects"
   end
 
   test "updating a Project" do
-    visit projects_url
+    visit project_url(@project)
+    # click_on "Show", match: :first
     click_on "Edit", match: :first
 
-    fill_in "Title", with: @project.title
+    fill_in "Title", with: "Hello Test"
     click_on "Update Project"
+    assert_selector "h1", text: "Hello Test"
 
     assert_text "Project was successfully updated"
-    click_on "Back"
+    click_on "All Projects"
   end
 
   test "destroying a Project" do
-    visit projects_url
+    visit project_url(@project)
+    # click_on "Show", match: :first
     page.accept_confirm do
       click_on "Destroy", match: :first
     end
+    assert_selector "h1", text: "Projects"
 
     assert_text "Project was successfully destroyed"
   end
