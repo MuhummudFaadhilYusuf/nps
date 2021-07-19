a1b6d0a13955f6ac1b52c1411743b45e5a16e449
Muhummud Faadhil Yusuf <muhummud@webhi.co>
2021-07-19 22:30:50 +0200

    added validation and tdd basics

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,3 +1,17 @@
 class Rating < ApplicationRecord
   belongs_to :person
+  validates :score, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
+
+  def promoter?
+  # [9,10].include?(score)
+    score >= 9 && score <= 10
+  end
+
+  def passive?
+    score >= 7 && score <= 8
+  end
+
+  def detractor?
+    score >= 0 && score <= 6
+  end
 end
--- test/models/rating_test.rb
+++ test/models/rating_test.rb
@@ -4,4 +4,84 @@ class RatingTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "valid if score is between 0 and 10" do
+    0.upto(10).each do |i|
+      rating = Rating.new(score: i)
+      rating.valid?
+      assert_empty rating.errors[:score]
+    end
+  end
+
+  test "invalid if less than 0" do
+    rating = Rating.new(score: -1)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
+
+  test "invalid if greater than 10"do
+    rating = Rating.new(score: 11)
+    rating.valid?
+    assert_not rating.errors[:score].empty?
+  end
+
+# promoter tests
+  test "promoter? returns true if score is 9 and 10" do
+    9.upto(10) do |i|
+      rating = Rating.new(score: i)
+      assert rating.promoter?
+    end
+  end
+
+  test "promoter? returns false if score is less than 9" do
+    rating = Rating.new(score: 1)
+    assert_not rating.promoter?
+  end
+
+  test "promoter? returns false if socre is greater than 10" do
+    rating = Rating.new(score: 11)
+    assert_not rating.promoter?
+  end
+
+# passive tests
+  test "passive? returns true if score is 7 or 8" do
+    7.upto(8) do |i|
+      rating = Rating.new(score: i)
+      assert rating.passive?
+    end
+  end
+
+  test "passive? returns false if score is less than 7" do
+    0.upto(6) do |i|
+      rating = Rating.new(score: i)
+      assert_not rating.passive?
+    end
+  end
+
+  test "passive? returns false if score is greater than 8" do
+    9.upto(10) do |i|
+      rating = Rating.new(score: i)
+      assert_not rating.passive?
+    end
+  end
+
+  # dectractor tests
+  test "detractor? returns true if score is less than or equal to 6" do
+    0.upto(6) do |i|
+      rating = Rating.new(score: i)
+      assert rating.detractor?
+    end
+  end
+
+  test "detractor? returns false if score is less than 0" do
+      rating = Rating.new(score: -1)
+      assert_not rating.detractor?
+  end
+
+  test "detractor? returns false if score is greater than 6" do
+    7.upto(10) do |i|
+      rating = Rating.new(score: i)
+      assert_not rating.detractor?
+    end
+  end
 end
