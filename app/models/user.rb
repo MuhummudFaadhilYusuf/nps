bd2867cb771ac4af089853bf42be4858d242cd91
Muhummud Faadhil Yusuf <muhummud@webhi.co>
2021-07-26 15:14:58 +0200

    added time tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :masqueradable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable
@@ -8,4 +9,12 @@ class User < ApplicationRecord
 
   has_many :notifications, as: :recipient
   has_many :services
+
+  before_create do
+    self.trial_ends_at = 7.days.from_now
+  end
+
+  def on_trial?
+    Time.current < trial_ends_at
+  end
 end
